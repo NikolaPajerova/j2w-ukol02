a5be67c9f70a4d07165304aa5286d8e01ccae10a
Niki <nicol@matfyz.cz>
2021-04-11 16:11:47 +0200

    Text se přepíná z daného seznamu, ale obrázku z Resource se nenačítají...

---
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/CitatKontroler.java
@@ -0,0 +1,30 @@
+package cz.czechitas.java2webapps.ukol2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class CitatKontroler {
+    private final Random random = new Random();
+
+    @GetMapping("/")        //
+    public ModelAndView nastavCitat() {
+        int nahodneCislo = random.nextInt(4);
+        List<String> seznamTextu = Arrays.asList("Existují pouze dva dny v roce, kdy nemůžeme udělat vůbec nic. Jedním je včerejšek a druhým zítřek.",
+                "Prvním krokem na cestě ke štěstí je učení se.","Studium bez touhy kazí paměť a neuchovává nic, co přijalo.",
+                "Příroda je zdrojem všech skutečných znalostí. Má svou vlastní logiku, své vlastní zákony, nemá žádný účinek bez příčiny ani vynález bez nutnosti.");
+
+        String nahodnyCitat = seznamTextu.get(nahodneCislo);
+        int nahodneCislo2 = random.nextInt(8)+1;
+
+        ModelAndView vysledek = new ModelAndView("citat");
+        vysledek.addObject("text", nahodnyCitat);
+        vysledek.addObject("obrazek", String.format("/images/image_%d.jpg",nahodneCislo2));
+        return vysledek;
+    }
+}
